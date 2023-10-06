5daba86e8e6c79fcaeb6397320278ac08a8388e2
Teodor Pytka <teodorpytka@gmail.com>
2023-10-06 14:07:46 +0200

    feat: Integrate tamagui

---
+++ README.md
@@ -0,0 +1,9 @@
+# About
+
+This projects goal to handle transactions in decent manner
+
+
+# TODOs
+
+ setup tamagui and create initial base
+
--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,15 @@ module.exports = function (api) {
     plugins: [
       // Required for expo-router
       'expo-router/babel',
+            // NOTE: this is optional, you don't *need* the compiler
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
     ],
   };
 };
